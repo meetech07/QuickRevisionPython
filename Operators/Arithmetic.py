3bd367f11cf58223a257272837cece92c1b38fa5
meetech07 <meetech07@gmail.com>
2026-06-13 17:18:46 +0530

    Arithmetic Operators

---
+++ Operators/Arithmetic.py
@@ -0,0 +1,30 @@
+# Arithmetic Operators
+
+# es Arithmetic Operators me oprance hota hai jise hm all Arithmetic workS KAR pate hi iase samjhte hi
+
+# Operator	Name	Example	 Result
+#    +	 Addition	10 + 3  	13
+#    -  Subtraction	10 - 3	    7
+#    *	 Multiplication	10 * 3	30
+#   /	Division	10 / 3	    3.333…
+#  //	Floor Division	10 // 3	    3
+#  %	Modulus (remainder)	10 % 3	1
+#  **	Exponentiation	2 ** 8	    256
+
+
+num1 = int(input("entar the fist number "));
+num2 = int(input("entar the second number "));
+
+print(num1 + num2);
+print(num1 - num2);
+print(num1 * num2);
+print(num1 / num2);
+print(num1 // num2);
+print(num1 % num2);
+print(num1 ** num2);
+
+
+
+
+
+   
