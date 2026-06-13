31f9c7f11afe0a958233935d1c424d29009eeb89
meetech07 <meetech07@gmail.com>
2026-06-13 18:07:18 +0530

    Add Que.

---
+++ Conditional/Questions.py
@@ -0,0 +1,21 @@
+# Q1 Accept two numbers and print the greatest between them.
+# Input: 14, 7
+# Output: 14 is greater
+# if 14 > 7:
+#     print(" 14 is greater")
+# else:
+#     print("Koi numabe nahi hai ")
+
+
+# Q2. Accept gender from user and print a greeting message.
+# Input: M
+# Output: Good Morning Sir
+
+# mag = input("please imtar the M ");
+# if mag == 'm'or mag == 'M':
+#     print("good Morning Manish");
+# else:
+#     print("please entar the M");
+
+
+
