a6d2683b906b61ed62ed1558510161fac018fdc4
meetech07 <meetech07@gmail.com>
2026-06-13 13:56:21 +0530

    Add string slicing

--- basic/1.py
+++ basic/1.py
@@ -64,6 +64,22 @@ print(name[-7]);
 #-11 -10 -9  -8  -7  -6  -5  -4  -3   -2  -1      negetive indexing name[-7] = s
 
 # String Slicing
+# Slicing cuts out a piece of a string. Syntax: string[start : stop : step] — note that stop index is 
+
+fist_name ="manish"
+print(fist_name[0:6:1]);
+print(fist_name[0:len(fist_name):1]);     # len ek lenght hai jise total index ka namuber nahi total no. of letar ya chatactor ko replsent karta hai 
+# formula or len() function.   = total indexs + 1 
+# ab iska matalb samjho fist_name[0:6:1]. 
+#  1. ye jo fist_name hai o ek variabul hai jo name ko holde kiya hai memory me 
+#  2.  fist_name[0:6:1]   yaha 0 ka matal start kaha se hoga Sting Slicing ke liye
+#  3.  fist_name[0:6:1]   yaha 6 ya len(fist_mane) ye bata hai ki kaha tak sring chahiye 
+#  4.  fist_name[0:6:1]  or final 1 ka matal ye hai ki kitna kitna gap ke bad sring chahiye matalb jaise c programing me pade hai i++
+
+
+
+   
+
 
 
 
