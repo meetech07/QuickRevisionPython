304105a235b2a74bc01fcd17cbf620f90d3bf479
meetech07 <meetech07@gmail.com>
2026-06-13 13:17:31 +0530

    first commit

---
+++ basic/1.py
@@ -0,0 +1,43 @@
+print("manish Singh");
+
+# Variables
+# Think of a variable as a labelled box — you put a value inside and refer to it by the label whenever you need it.
+name = "Manish"
+age  = 21
+city = "Sasaram"
+
+# Rules — these will cause errors:
+
+# ❌  1name = "x"  → can't start with a number
+# ❌  my name = "x"  → no spaces allowed
+# ❌  my-name = "x"  → no special characters (except underscore)
+
+
+# Naming Conventions
+# camelCase  → myVariableName
+# PascalCase → MyVariableName
+# snake_case → my_variable_name      this is a best nameing convention
+
+
+age =21;
+name = "manish Singh";
+#variyabul name printing do dypes line no. 6,7
+print(f"my name is {name} or mera age {age} etna hai ");
+print("my name is",name,"may ages is a",age);
+
+#data type
+"""What are Data Types?
+Every value in Python has a type that tells Python
+ what kind of data it is and what you can do with it. You don't need to declare types — Python figures it out automatically.
+
+"""
+a = 21;
+b = "manish";
+c = 3.43;
+d = True;
+
+print(type(a));
+print(type(b));
+print(type(c));
+print(type(d));
+
