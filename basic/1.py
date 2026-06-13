3745f9728c99a40c35dc16f6a06ed1362fd37fb1
meetech07 <meetech07@gmail.com>
2026-06-13 13:42:13 +0530

    Add indexing in sting

--- basic/1.py
+++ basic/1.py
@@ -1,3 +1,6 @@
+from tokenize import String
+
+
 print("manish Singh");
 
 # Variables
@@ -49,6 +52,19 @@ print(chr(64))   # → "@" (Character from Unicode)
 
 # the "ord" function the use the print a Unicode number and the synex is 
 # ord("pass the value")
+
+# String Indexing
+
+name = "ManishSingh";
+print(name[4]);
+print(name[-7]);
+
+# 0   1   2   3   4   5   6   7   8   9   10      positive indexing namr[4] = s
+# M   a   n   i   s   h   S   i   n   g   h
+#-11 -10 -9  -8  -7  -6  -5  -4  -3   -2  -1      negetive indexing name[-7] = s
+
+# String Slicing
+
 
 
 
