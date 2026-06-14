231ca63834235743cd7f95a143949aebf7f84be7
meetech07 <meetech07@gmail.com>
2026-06-14 19:28:20 +0530

    loop add

---
+++ loop/for.py
@@ -0,0 +1,15 @@
+# range() function is use the range of matalb ye kaha se lkaha tak jayega 
+# range(start,stop,skip) ise hm dekhenge ki kon kya karta hai 
+
+# range(start,stop,skip)  yaha start ka matalb ye hai ki hame kaha se strt karne kaha se karna hai.
+# range(start,stop,skip) or yaha stop ka matalb ye hai ki hmara range kaha rukna chahiye.
+# range(start,stop,skip) or skip ya jump ka matalb ye hai ki ek or dusre ke bich kiyna juam ya gap hona chahiye.
+# ab example dekhte hai.
+
+# range(stop)             # 0 up to stop-1
+# range(start, stop)      # start up to stop-1
+# range(start, stop, step)# start, jumping by step
+
+for i in range(10,21,1):
+    print(i)
+
---
+++ loop/while.py
@@ -0,0 +1,30 @@
+# while look 
+
+# The while loop keeps running as long as a condition is True. You use it when you don't know how many times you'll need to repeat.
+
+# count = 1;
+# while count <= 5:
+#     print(count);
+#     count += 1
+
+
+
+# isme ek infinty loop bhi hota hai aao dekhte hai 
+
+# while True:
+    # print("manish");
+
+
+# Infinite Loop Danger!
+# Always make sure your condition will eventually become False — otherwise your program runs forever
+
+# While loops also support
+# break  = matalb samjhao jaha lag gara wahi code stop ho jayeha puna look chale 
+
+# continue  = ye jaha lagega waha se ek age bad ke loop continua karega matalb ye 3 par laga hai to 4 se continua karega.
+
+
+count = 1;
+while count <= 5:
+    print(count);
+    count += 1
