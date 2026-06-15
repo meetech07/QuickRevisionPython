c413a115de625a8c2ab9d2749b9f78f99c111568
meetech07 <meetech07@gmail.com>
2026-06-15 19:32:03 +0530

    function

---
+++ function/basic.py
@@ -0,0 +1,19 @@
+# function
+
+def hlo():                                #function name?
+    print("Radha Radha")
+    print("Radha Radha")
+    print("Radha Radha")
+    print("Radha Radha")
+    print("Radha Radha")
+
+
+
+hlo()
+
+
+def add(a,b):   # paramitar 
+    print(a+b)
+
+
+add(10,20)   # argument  
