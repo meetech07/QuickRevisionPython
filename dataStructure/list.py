8bfbd0e3fda0f59beb3956e7d412fa96cad35b0a
meetech07 <meetech07@gmail.com>
2026-06-17 14:36:45 +0530

    liat add

---
+++ dataStructure/list.py
@@ -0,0 +1,25 @@
+# hm pale LIST dekhenge ki kya hota hai or methoda kya kya hota hai 
+# list is a collection which is ordered and changeable. Allows duplicate members.
+# list is defined by square brackets []
+# list items are indexed, the first item has index [0], the second item has index [1] etc.
+# list items can be of any data type
+# list items can be changed, added, and removed
+# list items can be duplicated
+# list items can be accessed by their index
+# list items can be sliced
+# list items can be looped through
+# list items can be checked if they exist in the list
+# list items can be counted
+# list items can be sorted
+# list items can be reversed
+# list items can be joined with other lists
+# list items can be copied
+# list items can be cleared
+
+# Examples 
+
+my_list = ["apple", "banana", "cherry"]
+print(my_list)
+# man lo hm koi ek ka name change karna chahte hai to hm index ka use karenge
+my_list[1] = "blueberry"
+print(my_list)
