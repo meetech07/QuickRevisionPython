a44e64901c5c75e556e180c6980461f8fca7ba60
meetech07 <meetech07@gmail.com>
2026-06-19 12:13:08 +0530

    update question on list

--- dataStructure/Questions.py
+++ dataStructure/Questions.py
@@ -47,17 +47,49 @@
 # samjho Average kya hota hai = total numabr ka sum /total numabr or object matal leg() function ka user sb samjhate hai 
 
 
-a = [10, 20, 30, 40]
+# a = [10, 20, 30, 40]
+# sum = 0
+# avr = 0
+
+# for i in a:
+#     sum = sum + i
+
+
+# print(f"after sum is total sum {sum}")
+# avr = sum/len(a)
+
+# print(f"aftar Avreges is a {avr}")
+
+#    __________________ab ise function se banayenge _______________________________________
 sum = 0
-avr = 0
+avg = 0 
 
-for i in a:
-    sum = sum + i
+# def avreg(a):
+#     global sum  # ab hm function ke bahar excex kar payenge.
+#     global avg
+#     # sum = 0
+#     # avg = 0
+#     for i in a:
+#         sum = sum + i
+#     print(f"the total sum is {sum}")
+#     avg = sum/len(a)
+#     print(f"total average is a {avg}")
 
 
-print(f"after sum is total sum {sum}")
-avr = sum/len(a)
+# user_input_list = input("entar the list of num ") # a of agument of the value
+# a = list(map(int, user_input_list.split()))   # map()  method me baad me dekhenge jb advance function padenge
+# avreg(a)    # call the function
 
-print(f"aftar Avreges is a {avr}")
 
-# ab ise function se banayenge 
+# print(sum)   # ise hm bahar exace nahi kar payenge kyu ki ek ye function ke andar hai matal ushka scop ush function tak simit rahega 
+# print(avg)   # ise hm bahar exace nahi kar payenge kyu ki ek ye function ke andar hai matal ushka scop ush function tak simit rahega 
+
+# gar hamko globul exacexe karna hai to hamko function ko batana padega ki ye variabul jo hm glopli user karenge matalb samjho ham use pure code me kahi bhi user kar sakte hai 
+
+
+#  Q3. Find the greatest element and print its index.
+# Input: [4, 8, 2, 9, 1]
+# Greatest = 9 at index 3
+
+
+# pahle samjhte hai ki kya karna hai hame ek pure lish me se ek element nikalna hai jo sab se bada hai or ushka index bhi find karna hai pahle bina function ke samjhate hai.
